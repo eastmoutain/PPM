e68e21dad5d9214e184a8de03ffe3c100296eb2f
east moutain <east.moutain.yang@gmail.com>
2016-06-01 09:59:44 +0800

    Update uart.c

--- BaseDrive/uart.c
+++ BaseDrive/uart.c
@@ -4,10 +4,10 @@
 void uart_init(void)
 {
     USART_InitTypeDef USART_InitStructure;
-	GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStructure;
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA , ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA , ENABLE);
     
     // TX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
@@ -15,7 +15,7 @@ void uart_init(void)
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     
-        // TX
+    // RX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
@@ -28,10 +28,9 @@ void uart_init(void)
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-	USART_Init(USART1, &USART_InitStructure);
-
-	/* Enable USART1 */
-	USART_Cmd(USART1, ENABLE);
+    USART_Init(USART1, &USART_InitStructure);
+    /* Enable USART1 */
+    USART_Cmd(USART1, ENABLE);
 }
 
 /*
@@ -44,9 +43,7 @@ int fputc(int ch, FILE *f)
 	USART_SendData(USART1, (uint8_t) ch);
 
 	/* Loop until the end of transmission */
-	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-	{}
-
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
 	return ch;
 }
 
